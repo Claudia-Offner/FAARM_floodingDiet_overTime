abc304c0a1243650fa97f18a97c245e34dde5cbb
ucesccf <ucesccf@ucl.ac.uk>
2021-12-01 09:41:19 +0000

    ADD: subset for preliminary modelling

--- main.py
+++ main.py
@@ -43,3 +43,34 @@ for name, group in groups:
     newage_index = list(end.index)
     for i in newage_index:
         df['age'][i] = age
+
+# ====================================================================================
+# SUBSET FOR PRELIMINARY ANALYSIS
+# ====================================================================================
+
+# Create cumulative variable for flooding
+df['c_flood'] = df[['wcode', 'Cluster_Diff']].groupby('wcode').cumsum()
+df['r_flood'] = df[['wcode', 'Region_Diff']].groupby('wcode').cumsum()
+
+# Subset to necessary variables
+data = df[['wcode', 'c_code', 'panel', 'treatment', 'c_flood', 'r_flood', 'md_score', 'md_scale',
+           'dd10r_score', 'dd10r_min', 'hfias_score', 'hfias', 'hfias_cat', 'hfias_d', 'preg', 'religion',
+           'education', 'age', 'terc']]  # use md_scale, dd10r_min, hfias_cat
+
+
+# Separate EL and BL
+bl = data.loc[data['panel'] == 'base']
+keep_same = {'wcode', 'c_code', 'panel', 'treatment'}
+bl.columns = ['{}{}'.format(c, '' if c in keep_same else '_BL') for c in bl.columns]
+bl = bl.drop(['panel'], axis=1)
+
+el = data.loc[data['panel'] == 'end']
+keep_same = {'wcode', 'c_code', 'panel', 'treatment'}
+el.columns = ['{}{}'.format(c, '' if c in keep_same else '_EL') for c in el.columns]
+el = el.drop(['panel'], axis=1)
+
+# Merge EL and BL again
+subset = pd.merge(bl, el, how='right', on=['wcode', 'c_code', 'treatment'])
+
+#%%
+subset.isnull().sum()
