81b32661ebec9989a1fe54b4a00cffcfd4219370
Claudia Offner <72859960+Claudia-Offner@users.noreply.github.com>
2022-07-27 13:41:33 +0100

    Removed: Flooding function to main code body

--- 2_Python/P1_GEE_Compilation.py
+++ 2_Python/P1_GEE_Compilation.py
@@ -26,73 +26,71 @@ def retrieve_name(var):
     return [var_name for var_name, var_val in callers_local_vars if var_val is var]
 
 
-def get_flood(file_names):
-    df = pd.DataFrame(columns=['c_code', 'panel', 'dov', 'c_Areakm2', 'c_floodedAreakm2', 'r_floodedAreakm2', 'r_max',
-                               'r_min', 'r_mean', 'r_sd'])
-
-    props = ['date', 'panel', 'r_floodedAreakm2', 'r_max', 'r_min', 'r_mean', 'r_sd']
-    nprops = ['enum_c_cod', 'c_Areakm2', 'c_floodedAreakm2']
-    nfeat = 'clusters'
-
-    for f in file_names:
-        result = Extractor(f).nested_json_to_df(main_props=props, nested_feature=nfeat, nested_props=nprops)
-        df = df.append(result, ignore_index=True)
-
-    df['panel'] = df['panel'].str.replace('P9', 'end')
-
-    return df
-
-
+#%%
+# ========================================================================================
+# GET BOUNDING BOX (for GEE)
 # ========================================================================================
 # Create Bounding Box from FAARM 1km Buffer file (for G1_compReference.js)
+os.chdir(data_path)
+
+# Reproject to a projected coordinate system in meters (32646), for precise 1km buffer
+x = SpatialProcessor('FAARM/enum_cCode_buffer1000.shp').create_bbox(crs=32646, buffer=1000, plot=False)
+y = SpatialProcessor(x).transform_df(crs=4326)  # Transform back to WSG84
+y.to_file('FAARM/FAARM1km_bbox_CO.shp')  # Export
+
+#%%
 # ========================================================================================
-# os.chdir(data_path)
-#
-#
-# # Reproject to a projected coordinate system in meters (32646), for precise 1km buffer
-# x = SpatialProcessor('FAARM/enum_cCode_buffer1000.shp').create_bbox(crs=32646, buffer=1000, plot=False)
-# y = SpatialProcessor(x).transform_df(crs=4326) # Transform back to WSG84
-# y.to_file('FAARM/FAARM1km_bbox_CO.shp')  # Export
-
-
+# GET REFERENCE STATS (for GEE)
 # ========================================================================================
 # Convert Composite Reference Statistics to CSV for selection (from G1_compReference.js)
-# ========================================================================================
-# main_prop = ['Date', 'Maximum', 'Minimum', 'Mean', 'Stdev']
-# y = Extractor('ReferenceSelection.geojson').json_to_df(main_props=main_prop)
-# y.to_csv('ReferenceSelection.csv', index=False)
 
+main_prop = ['Date', 'Maximum', 'Minimum', 'Mean', 'Stdev']
+y = Extractor('ReferenceSelection.geojson').json_to_df(main_props=main_prop)
+y.to_csv('ReferenceSelection.csv', index=False)
 
+#%%
 # ====================================================================================
-# GEE FLOODING EXPOSURE
+# GEE FLOODING EXPOSURE (for FAARM)
 # ====================================================================================
-# # Load satellite image data from JSON file to dataframe, clean images and export to csv
-# os.chdir(data_path + '/GEE_Flooding_Clusters')
-#
-# surv = ['P1.geojson', 'P2.geojson', 'P3.geojson', 'P4.geojson', 'P5.geojson',
-#         'P6.geojson', 'P7.geojson', 'P8.geojson', 'P9.geojson', 'end.geojson']
-#
-# flood_df = get_flood(surv)
-# flood_df['dov'] = pd.to_datetime(flood_df['dov'], errors='coerce')
-# flood_df = Panelist(flood_df).get_dd_mm_yyyy()
-#
-# # Calculate Flood Percentage by Cluster
-# flood_df['perc_flooded'] = flood_df['c_floodedAreakm2'] / flood_df['c_Areakm2']
-#
-# # Flood mean grouped by wcode, year, month
-# flood_df['month2'] = flood_df['month'].apply(lambda x: '{0:0>2}'.format(x))
-# flood_df['year_month'] = flood_df['year'].astype(str) + '-' + flood_df['month2'].astype(str)
-# flood_df = flood_df.groupby(['c_code', 'year_month'], as_index=False).mean()  # panel
-# flood_df = flood_df.sort_values(by=['c_code', 'year_month']).reset_index().drop('index', axis=1)  # important for lag
-# flood_df.drop('day', axis=1, inplace=True)
-#
-# # Save data
-# os.chdir(data_path)
-# flood_df.to_csv('gee_flood_df.csv', index=False)
-#
+# Load satellite image data from JSON file to dataframe, clean images and export to csv
+os.chdir(data_path + '/GEE_Flooding_Clusters')
 
+file_names = ['P1.geojson', 'P2.geojson', 'P3.geojson', 'P4.geojson', 'P5.geojson',
+              'P6.geojson', 'P7.geojson', 'P8.geojson', 'P9.geojson', 'end.geojson']
+
+flood_df = pd.DataFrame(columns=['c_code', 'panel', 'dov', 'c_Areakm2', 'c_floodedAreakm2', 'r_floodedAreakm2', 'r_max',
+                                 'r_min', 'r_mean', 'r_sd'])
+
+props = ['date', 'panel', 'r_floodedAreakm2', 'r_max', 'r_min', 'r_mean', 'r_sd']
+nprops = ['enum_c_cod', 'c_Areakm2', 'c_floodedAreakm2']
+nfeat = 'clusters'
+
+for f in file_names:
+    result = Extractor(f).nested_json_to_df(main_props=props, nested_feature=nfeat, nested_props=nprops)
+    flood_df = flood_df.append(result, ignore_index=True)
+
+flood_df['panel'] = flood_df['panel'].str.replace('P9', 'end')
+
+flood_df['dov'] = pd.to_datetime(flood_df['dov'], errors='coerce')
+flood_df = Panelist(flood_df).get_dd_mm_yyyy()
+
+# Calculate Flood Percentage by Cluster
+flood_df['perc_flooded'] = flood_df['c_floodedAreakm2'] / flood_df['c_Areakm2']
+
+# Flood mean grouped by wcode, year, month
+flood_df['month2'] = flood_df['month'].apply(lambda m: '{0:0>2}'.format(m))
+flood_df['year_month'] = flood_df['year'].astype(str) + '-' + flood_df['month2'].astype(str)
+flood_df = flood_df.groupby(['c_code', 'year_month'], as_index=False).mean()  # panel
+flood_df = flood_df.sort_values(by=['c_code', 'year_month']).reset_index().drop('index', axis=1)  # important for lag
+flood_df.drop('day', axis=1, inplace=True)
+
+# Save data
+os.chdir(data_path)
+flood_df.to_csv('gee_flood_df.csv', index=False)
+
+#%%
 # ====================================================================================
-# GEE ENVIRONMENT CONTROLS
+# GEE ENVIRONMENT CONTROLS (for FAARM)
 # ====================================================================================
 os.chdir(data_path)
 
@@ -120,7 +118,7 @@ prec.to_csv('gee_prec_df.csv', index=False)
 
 
 # ====================================================================================
-# DATA CLEANING & FORMATTING
+# DATA CLEANING & FORMATTING (for FAARM)
 # ====================================================================================
 os.chdir(data_path)
 
@@ -147,8 +145,8 @@ for i in df_list:
     min_name = retrieve_name(i)[0] + '_min'
     max_name = retrieve_name(i)[0] + '_max'
     i.rename(columns={'mean': mean_name, 'min': min_name, 'max': max_name}, inplace=True)
-    # Make single digits double
-    i['month'] = i['month'].astype(int).apply(lambda x: '{0:0>2}'.format(x))
+    # Make single digits double (i.e. 01, 02, etc.)
+    i['month'] = i['month'].astype(int).apply(lambda m: '{0:0>2}'.format(m))
     # year_month column
     i['year_month'] = i['year'].astype(str) + '-' + i['month'].astype(str)
     i.drop('day', axis=1, inplace=True)
